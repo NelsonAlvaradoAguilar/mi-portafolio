2b390963fce3f28e03bd7499bb510b059f527e17
NelsonAlvaradoAguilar <nelsonalvaradoaguilar@gmail.com>
2024-10-20 16:20:53 -0400

    commiting aboutMe section test typescript and vite tecnologies

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
  import AboutMe from './components/AboutMe/AboutMe'
 import { aboutMeData } from './apiData/ApiData'
