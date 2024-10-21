20ae89258793f619da40ca646d8b188aef84834f
NelsonAlvaradoAguilar <nelsonalvaradoaguilar@gmail.com>
2024-10-21 03:01:25 -0400

    testing types, education section, rendering with vite building with react and typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
 import { useState } from 'react'
 
-import './App.css'
+import './App.scss'
  import AboutMe from './components/AboutMe/AboutMe'
-import { aboutMeData } from './apiData/ApiData'
+import { aboutMeData, educationData, educationDataTitles } from './apiData/ApiData'
+import Education from './components/EducationDetails/EducationDetails'
 function App() {
   const [count, setCount] = useState(0)
- 
+type likes ={
+  likes:Number
+}
+type likesdata={
+  likes:likes[]
+}
+
+ function handleLikes({likes}:likesdata) {
+  setCount(prevCount => prevCount + 1);
+ }
   return (
     <>
       <>
       <div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + TypeScript</h1>
       </div>
       <AboutMe aboutMeData={aboutMeData}/>
    
-    
-      <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <Education educationData={educationData} titles={educationDataTitles} />
+    <button onClick={() => handleLikes({ likes: [{ likes: count }] })}>
+       Likes {count}
+      </button>
     </>
   
     
--- src/apiData/ApiData.tsx
+++ src/apiData/ApiData.tsx
@@ -8,7 +8,51 @@ const aboutMeData = [
     }
    
   ];
+  const educationData = [
+    {
+      id: 0,
+      title: "BrainStation 2024",
+      school: "BrainStation",
+      type: " | Diploma",
+      career: "Software Egineer",
+      city: "TORONTO, ON",
+      image: "kjnkdf",
+      date: "Set 2023 - Apr 2024",
+    },
+    {
+      id: 1,
+      title: "BrainStation 2023",
+      school: "BrainStation",
+      type: " | Certification",
+      career: "Web Development",
+      city: "TORONTO, ON",
+      image: "jhkjh",
+      date: "August 2 - 2023 ",
+    },
+    {
+      id: 2,
+      title: "National training Academy",
+      school: "National training Academy",
+      type: " | Certification",
+      career: "Financial Management.",
+      city: "COLOMBIA",
+      image: "hhgkhghj",
+      date: "Jan 2011 - Dec 2012",
+    },
+  ];
 
-
-
-  export {aboutMeData}
+  const educationDataTitles = [
+    {
+      id: 0,
+      title: "BrainStation 2024",
+    },
+    {
+      id: 1,
+      title: "BrainStation 2023",
+    },
+    {
+      id: 2,
+      title: "National training Academy, Colombia",
+    },
+  ];
+  export {aboutMeData,educationData,educationDataTitles}
