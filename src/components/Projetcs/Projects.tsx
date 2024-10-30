18b30b19a712398cf660efe101b9338874664d0d
NelsonAlvaradoAguilar <nelsonalvaradoaguilar@gmail.com>
2024-10-29 22:23:38 -0400

    fonts

---
+++ src/components/Projetcs/Projects.tsx
@@ -0,0 +1,13 @@
+import { ProjectsProps } from "../../apiData/Types";
+import Titles from "../Titles/Titles";
+
+
+function ProjectList ({titles}:ProjectsProps) {
+    return(<>
+
+    </>)
+}
+
+
+
+export default ProjectList;
